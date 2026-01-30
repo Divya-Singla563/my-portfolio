da73c57a10ef0da24d63afbe4d88e4fa0c87538c
Divya Singla <divyasingla493@gmail.com>
2026-01-30 13:34:30 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ import * as Sections from "@/components/sections";
 export default function Home() {
   return (
     <main>
-      <section id="home" className="relative flex flex-col lg:flex-row pt-16 sm:pt-20">
+      <section
+        id="home"
+        className="relative flex flex-col lg:flex-row pt-16 sm:pt-20"
+      >
         <Sections.ProfileSidebar />
 
         {/* Right Content Area - Scrollable */}
         <div className="w-full lg:flex-1">
           <div className="min-h-screen px-6 sm:px-10 lg:px-16 xl:px-20 lg:pl-10 xl:pl-7 py-8 sm:py-12 lg:py-24 lg:pt-20">
-
             <Sections.Introduction />
-
+            {/* <UserCircle className="w-6 h-6 " /> */}
             <div id="about">
               <Sections.AboutMe />
             </div>
