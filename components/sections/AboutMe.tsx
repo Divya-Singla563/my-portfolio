a6fa3592248f11490927f90b55983df76abc3ac7
Divya Singla <divyasingla493@gmail.com>
2026-01-30 13:35:24 +0530

    changes

--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -1,32 +1,36 @@
 "use client";
 
-import { SITE } from "@/lib/constants";
 import { motion } from "framer-motion";
 import { UserCircle } from "lucide-react";
 
 export default function AboutMe() {
-    return (
-        <section className="w-full py-6 md:py-12">
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.4, duration: 0.5 }}
-            >
-                <div className="flex items-center gap-3 mb-6">
-                    <UserCircle className="w-6 h-6 " />
-                    <h3 className="text-sm font-semibold uppercase tracking-wider text-[var(--muted)]">
-                        About
-                    </h3>
-                </div>
+  return (
+    <section className="w-full py-6 md:py-12">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+      >
+        <div className="flex items-center gap-3 mb-6">
+          <UserCircle className="w-6 h-6 " />
+          <h3 className="text-sm font-semibold uppercase tracking-wider text-[var(--muted)]">
+            About
+          </h3>
+        </div>
 
-                <h2 className="text-4xl sm:text-5xl font-bold text-[var(--foreground)] mb-6">
-                    About <span className="text-[var(--accent)]">Me</span>
-                </h2>
+        <h2 className="text-4xl sm:text-5xl font-bold text-[var(--foreground)] mb-6">
+          About <span className="text-[var(--accent)]">Me</span>
+        </h2>
 
-                <p className="text-lg text-[var(--muted)] max-w-3xl leading-relaxed mb-6">
-                    Full Stack Developer with 3+ years of hands-on experience in designing, developing, and deploying scalable web applications. Proficient in front-end technologies like React Native, React JS, Javascript, HTML, CSS, SCSS and skilled in back-end development using Node.js and Express. Experienced in building RESTful APIs, integrating databases, and optimizing application performance.
-                </p>
-            </motion.div>
-        </section>
-    );
+        <p className="text-lg text-[var(--muted)] max-w-3xl leading-relaxed mb-6">
+          Full Stack Developer with 3+ years of hands-on experience in
+          designing, developing, and deploying scalable web applications.
+          Proficient in front-end technologies like React Native, React JS,
+          Javascript, HTML, CSS, SCSS and skilled in back-end development using
+          Node.js and Express. Experienced in building RESTful APIs, integrating
+          databases, and optimizing application performance.
+        </p>
+      </motion.div>
+    </section>
+  );
 }
